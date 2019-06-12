bf47640a54f596bf6793645f2a8c8fa72ff1bffc
Ronith <ronith900@gmail.com>
2019-06-12 23:46:47 +0800

    notfound commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import NavBar from "./components/common/navBar";
-import {Route,Switch,Redirect} from 'react-router-dom';
-import Customers from './components/common/customers';
-import Rentals from './components/common/rentals';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/common/customers";
+import Rentals from "./components/common/rentals";
+import NotFound from "./components/common/notFound";
 
 class App extends Component {
   render() {
@@ -12,11 +13,12 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/" exact component={Movies}></Route>
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Movies} />
+            <Redirect to="/not-found" />
           </Switch>
-
         </div>
       </div>
     );
