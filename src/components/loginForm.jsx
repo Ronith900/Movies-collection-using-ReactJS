7c34bb8d590a6ed0707a5cc9216155b7a4dde8a4
Ronith <ronith900@gmail.com>
2019-06-16 13:39:34 +0800

    login form commit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,16 +8,22 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   validate = () => {
+    const option = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, option);
+    if (!error) return null;
     const errors = {};
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
   handleSubmit = e => {
     e.preventDefault();
@@ -24,12 +31,26 @@ class LoginForm extends Component {
 
     const errors = this.validate();
     this.setState({ errors: errors || {} });
+    if (errors) return;
+  };
+
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is empty";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "password is empty";
+    }
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
